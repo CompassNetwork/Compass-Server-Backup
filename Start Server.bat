80495977646060748aa9170a09e84f6640231726
David Tucker <compassserverbuisness@gmail.com>
2016-07-31 17:54:30 -0500

    Server Files Upload
    Added all Server Files.

---
+++ Start Server.bat
@@ -0,0 +1,11 @@
+@echo off
+title Compass Network Server [Bukkit 1.10.2]
+color b
+cls
+java -Xmx1g -jar craftbukkit.jar -o true
+echo.
+echo ---------------------------------------------------------------------------------------
+echo Press any key to end console //
+echo ---------------------------------------------------------------------------------------
+PAUSE>nul
+exit()
